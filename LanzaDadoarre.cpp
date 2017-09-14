5df161cb2a539bb3a72687394cb3c94154d7d051
LuisTorres0709 <31056953+LuisTorres0709@users.noreply.github.com>
2017-09-14 16:19:55 -0500

    Update LanzaDadoarre.cpp

--- LanzaDadoarre.cpp
+++ LanzaDadoarre.cpp
@@ -10,35 +10,11 @@ int lanzaDadito();
 int main()
 {
 	srand(time(NULL));
-	int dado, cont[6];
-	for (int i = 0; i < 6; i++) {
-		cont[i] = 0;
-	}
+	int dado, cont[6]={0};
 	cout << "Lanzamos el dado 100 veces para contabilizar cuantas veces cae cada cara\nLanzando..." << endl;
 	for (int i = 1; i <= 100; i++) {
 		dado = lanzaDadito();
-		switch (dado) {
-		case 1:
-			cont[0]++;
-			break;
-		case 2:
-			cont[1]++;
-			break;
-		case 3:
-			cont[2]++;
-			break;
-		case 4:
-			cont[3]++;
-			break;
-		case 5:
-			cont[4]++;
-			break;
-		case 6:
-			cont[5]++;
-			break;
-		default:
-			break;
-		}
+		cont[dado-1]++;
 	}
 	cout << "La cara 1 salio: " << cont[0] << " veces..." << endl;
 	cout << "La cara 2 salio: " << cont[1] << " veces..." << endl;

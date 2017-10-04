e7d4e077d78e31d870f172744f1aabb061217399
LuisTorres0709 <31056953+LuisTorres0709@users.noreply.github.com>
2017-10-04 16:32:59 -0500

    Update VectoresParaSistemas.cpp

--- VectoresParaSistemas.cpp
+++ VectoresParaSistemas.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void menu();
+int menu();
 void agregar(int vector[],int num,int &tope);
 void mostrar(int vector[], int tope);
-int buscar(int vector[], int num);
-void eliminar(int vector[], int num);
-void insertar(int vector[], int num, int posic, int tope);
+int buscar(int vector[], int num,int tope);
+void eliminar(int vector[], int num, int &tope);
+void insertar(int vector[], int num, int posic, int &tope);
 
 int main(){
     int vector[15]={0};
     int num,posic, tope=0, opcion;
     do{
-    menu();
-    cin>>opcion;
+    opcion=menu();
     switch(opcion){
     case 1:
         cout<<"Inserta Numero: ";
@@ -26,12 +25,17 @@ int main(){
     case 3:
         cout<<"Que numero buscas? ";
         cin>>num;
-        cout<<"El numero "<<num<<" Esta en la posicion "<<buscar(vector, num)+1;
+        if((buscar(vector, num, tope))<=0){
+            cout<<"El numero "<<num<<" Esta en la posicion "<<buscar(vector, num, tope)+1;
+        }
+        else{
+            cout<<"No se encuentra ese numero..."<<endl;
+        }
         break;
     case 4:
         cout<<"Inserta Numero a borrar: ";
         cin>>num;
-        eliminar(vector,num);
+        eliminar(vector,num, tope);
         break;
     case 5:
         cout<<"Inserta Numero: ";
@@ -46,14 +50,17 @@ int main(){
     }while(opcion!=0);
 }
 
-void menu(){
-    cout<<"Menu \n"<<
+int menu(){
+    int opcion=0;
+    cout<<endl<<"Menu \n"<<
         "1.- Agrega un numero al vector... \n"<<
         "2.- Muestra el vector... \n"<<
         "3.- Busca un valor en el vector... \n"<<
         "4.- Elimina un valor del vector... \n"<<
         "5.- Inserta numero... \n"<<
         "Presiona 0 para salir... \n";
+    cin>>opcion;
+    return opcion;
 }
 void agregar(int vector[],int num,int &tope){
     if(tope<=14){
@@ -62,28 +69,37 @@ void agregar(int vector[],int num,int &tope){
     }
 }
 void mostrar(int vector[], int tope){
-    for(int i=0; i<=tope; i++){
+    for(int i=0; i<tope; i++){
         cout<<vector[i]<<" En la posicion "<<i+1<<endl;
     }
 }
-int buscar(int vector[], int num){
-    int posicionalv;
-    for(int i=14; i>=0; i--){
+int buscar(int vector[], int num,int tope){
+    int posicionalv=-1;
+    for(int i=tope; i>=0; i--){
         if(vector[i]==num){
             posicionalv=i;
+
         }
     }
- return posicionalv;
+    return posicionalv;
 }
-void eliminar(int vector[], int num){
-    for(int i=0; i<15; i++){
+void eliminar(int vector[], int num,int &tope){
+    for(int i=0; i<=tope; i++){
         if(vector[i]==num){
             vector[i]=0;
+            for(int j=i; j<=tope; j++){
+                vector[j]=vector[i+1];
+            }
+            tope--;
         }
     }
 }
-void insertar(int vector[], int num, int posic,int tope){
+void insertar(int vector[], int num, int posic,int &tope){
     if(posic<=tope){
-    vector[posic-1]=num;
+        for(int i=tope; i>(posic-1); i++){
+            vector[i+1]=vector[i];
+        }
+        tope++;
     }
+
 }
